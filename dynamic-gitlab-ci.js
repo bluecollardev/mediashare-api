62a51957f25f98ca609a6a48ba04bd056f710294
Lucas Lopatka <lucas@bluecollardev.com>
2024-03-24 01:58:38 +0700

    Adjust gitlab ci and js.

--- dynamic-gitlab-ci.js
+++ dynamic-gitlab-ci.js
@@ -1,7 +1,7 @@
 const { writeFileSync } = require('fs');
 
-const createBaseFile = () => `
-stages:
+const createBaseFile = () =>
+`stages:
   - publish
 `;
 
@@ -12,7 +12,7 @@ publish:empty:
     - echo 'no apps affected!'
 `;
 
-const createJob = (service) => `
+const createJob = (service, env) => `
 publish:${service}:
   stage: publish
   image: docker
@@ -25,24 +25,22 @@ publish:${service}:
   before_script:
     - docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
   script:
-    - docker build --pull --cache-from $CI_REGISTRY_IMAGE --tag $CI_REGISTRY_IMAGE/${service}-dev:$TAG_NAME .
+    - docker build --pull --cache-from $CI_REGISTRY_IMAGE --tag $CI_REGISTRY_IMAGE/${env === 'dev' ? service + '-dev' : service}:$TAG_NAME .
     - docker push $CI_REGISTRY_IMAGE/${service}-dev:$TAG_NAME
 `;
 
-const createCIFile = (projects) => {
-  if (!projects.length) {
+const createCIFile = (services, env) => {
+  if (!services.length) {
     return createBaseFile().concat(createEmptyJob());
   }
 
-  return createBaseFile().concat(projects.map(createJob).join('\n'));
+  return createBaseFile().concat(services.map((service) => createJob(service, env)).join('\n'));
 };
 
 const createDynamicGitLabFile = () => {
-  const [stringifiedAffected] = process.argv.slice(2);
-
-  const { projects } = JSON.parse(stringifiedAffected);
-
-  const content = createCIFile(projects);
+  const args = process.argv.slice(2, 4);
+  const [projects, env] = args;
+  const content = createCIFile(JSON.parse(projects), env);
 
   writeFileSync('dynamic-gitlab-ci.yml', content);
 };
